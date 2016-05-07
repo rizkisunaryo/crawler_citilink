5b4da2ccb21e5480eb7c578c13fca5bef44d5645
rizkisunaryo <rizkisunaryo@gmail.com>
2016-05-07 14:21:45 +0700

    update

--- post_passenger.php
+++ post_passenger.php
@@ -41,50 +41,50 @@ if ( ( !isset($req->adult_child_passengers) || count($req->adult_child_passenger
 
 
 
-$inpBody = "__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUBMGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgIFR0NPTlRST0xHUk9VUFBBU1NFTkdFUiRQYXNzZW5nZXJJbnB1dFZpZXdQYXNzZW5nZXJWaWV3JENoZWNrQm94SW5zdXJhbmNlBUFDT05UUk9MR1JPVVBQQVNTRU5HRVIkUGFzc2VuZ2VySW5wdXRWaWV3UGFzc2VuZ2VyVmlldyRDaGVja0JveFBNSXZkWh6Cdtm1mad5oP%2B7VGz2nQKe&pageToken=&";
+$inpBody = '__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUBMGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgIFR0NPTlRST0xHUk9VUFBBU1NFTkdFUiRQYXNzZW5nZXJJbnB1dFZpZXdQYXNzZW5nZXJWaWV3JENoZWNrQm94SW5zdXJhbmNlBUFDT05UUk9MR1JPVVBQQVNTRU5HRVIkUGFzc2VuZ2VySW5wdXRWaWV3UGFzc2VuZ2VyVmlldyRDaGVja0JveFBNSXZkWh6Cdtm1mad5oP%2B7VGz2nQKe&pageToken=&';
 
 $inp = array();
 
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24DropDownListTitle'] = $reqAddressPerson->title;
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24TextBoxFirstName'] = $reqAddressPerson->first_name;
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24TextBoxLastName'] = $reqAddressPerson->last_name;
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24TextBoxAddressLine1'] = $reqAddressPerson->address;
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24TextBoxCity'] = $reqAddressPerson->city;
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24TextBoxWorkPhone'] = $reqAddressPerson->phone;
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24TextBoxOtherPhone'] = $reqAddressPerson->phone;
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24TextBoxHomePhone'] = $reqAddressPerson->phone;
-$inp['CONTROLGROUPPASSENGER%24ContactInputPassengerView%24TextBoxEmailAddress'] = $reqAddressPerson->email_address;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$DropDownListTitle'] = $reqAddressPerson->title;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$TextBoxFirstName'] = $reqAddressPerson->first_name;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$TextBoxLastName'] = $reqAddressPerson->last_name;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$TextBoxAddressLine1'] = $reqAddressPerson->address;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$TextBoxCity'] = $reqAddressPerson->city;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$TextBoxWorkPhone'] = $reqAddressPerson->phone;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$TextBoxOtherPhone'] = $reqAddressPerson->phone;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$TextBoxHomePhone'] = $reqAddressPerson->phone;
+$inp['CONTROLGROUPPASSENGER$ContactInputPassengerView$TextBoxEmailAddress'] = $reqAddressPerson->email_address;
 
 foreach($req->adult_child_passengers as $k=>$v) {
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListTitle_'.$k] = $v->title;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24TextBoxFirstName_'.$k] = $v->first_name;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24TextBoxLastName_'.$k] = $v->last_name;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24TextBoxMiddleName_'.$k] = 'middle';
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListBirthDateDay_'.$k] = $v->birth_date_day;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListBirthDateMonth_'.$k] = $v->birth_date_month;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListBirthDateYear_'.$k] = $v->birth_date_year;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListDocumentDateDay0_'.$k] = 1;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListDocumentDateMonth0_'.$k] = 1;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListDocumentDateYear0_'.$k] = 1;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListTitle_'.$k] = $v->title;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$TextBoxFirstName_'.$k] = $v->first_name;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$TextBoxLastName_'.$k] = $v->last_name;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$TextBoxMiddleName_'.$k] = 'middle';
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListBirthDateDay_'.$k] = $v->birth_date_day;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListBirthDateMonth_'.$k] = $v->birth_date_month;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListBirthDateYear_'.$k] = $v->birth_date_year;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListDocumentDateDay0_'.$k] = 1;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListDocumentDateMonth0_'.$k] = 1;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListDocumentDateYear0_'.$k] = 1;
 }
 
 foreach($req->infant_passengers as $k=>$v) {
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListAssign_'.$k.'_'.$k] = $k;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24TextBoxFirstName_'.$k.'_'.$k] = $v->first_name;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24TextBoxLastName_'.$k.'_'.$k] = $v->last_name;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListGender_'.$k.'_'.$k] = $v->gender;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24TextBoxMiddleName_'.$k.'_'.$k] = 'middle';
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListBirthDateDay_'.$k.'_'.$k] = $v->birth_date_day;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListBirthDateMonth_'.$k.'_'.$k] = $v->birth_date_month;
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24DropDownListBirthDateYear_'.$k.'_'.$k] = $v->birth_date_year;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListAssign_'.$k.'_'.$k] = $k;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$TextBoxFirstName_'.$k.'_'.$k] = $v->first_name;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$TextBoxLastName_'.$k.'_'.$k] = $v->last_name;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListGender_'.$k.'_'.$k] = $v->gender;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$TextBoxMiddleName_'.$k.'_'.$k] = 'middle';
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListBirthDateDay_'.$k.'_'.$k] = $v->birth_date_day;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListBirthDateMonth_'.$k.'_'.$k] = $v->birth_date_month;
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$DropDownListBirthDateYear_'.$k.'_'.$k] = $v->birth_date_year;
 }
 
 if (isset($req->pmi) && $req->pmi==1) {
-    $inp['CONTROLGROUPPASSENGER%24PassengerInputViewPassengerView%24CheckBoxPMI'] = 'on';
+    $inp['CONTROLGROUPPASSENGER$PassengerInputViewPassengerView$CheckBoxPMI'] = 'on';
 }
 
 $inpBody .= http_build_query($inp);
-$inpBody .= "&CONTROLGROUPPASSENGER%24ItineraryDistributionInputPassengerView%24Distribution=2&CONTROLGROUPPASSENGER%24ButtonSubmit=Lanjutkan";
+$inpBody .= '&CONTROLGROUPPASSENGER$ItineraryDistributionInputPassengerView$Distribution=2&CONTROLGROUPPASSENGER$ButtonSubmit=Lanjutkan';
 
 echo $inpBody;
 
@@ -103,19 +103,19 @@ $arrContextOptions=
     );
 file_get_contents("https://book.citilink.co.id/Passenger.aspx", false, stream_context_create($arrContextOptions));
 
-$arrContextOptions=
-    array(
-        'http' => array(
-            'header'  => "Cookie: ASP.NET_SessionId=".$req->session.";\r\n" .
-                "Host: book.citilink.co.id\r\n" .
-                "Content-Type: application/x-www-form-urlencoded\r\n" .
-                "https://book.citilink.co.id/Passenger.aspx\r\n",
-            'method'  => 'GET',
-        ),
-    );
-$response = file_get_contents("https://book.citilink.co.id/Insurance.aspx", false, stream_context_create($arrContextOptions));
-
-$response = str_replace("images/", "https://book.citilink.co.id/images/", $response);
-$response = str_replace("css/", "https://book.citilink.co.id/css/", $response);
-$response = str_replace("js/", "https://book.citilink.co.id/js/", $response);
-echo $response;
+//$arrContextOptions=
+//    array(
+//        'http' => array(
+//            'header'  => "Cookie: ASP.NET_SessionId=".$req->session.";\r\n" .
+//                "Host: book.citilink.co.id\r\n" .
+//                "Content-Type: application/x-www-form-urlencoded\r\n" .
+//                "https://book.citilink.co.id/Passenger.aspx\r\n",
+//            'method'  => 'GET',
+//        ),
+//    );
+//$response = file_get_contents("https://book.citilink.co.id/Insurance.aspx", false, stream_context_create($arrContextOptions));
+//
+//$response = str_replace("images/", "https://book.citilink.co.id/images/", $response);
+//$response = str_replace("css/", "https://book.citilink.co.id/css/", $response);
+//$response = str_replace("js/", "https://book.citilink.co.id/js/", $response);
+//echo $response;
